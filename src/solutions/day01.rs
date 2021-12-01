7a3b0143e5a28c9c72f481da60a5951618ade83b
Eldritch Cheese <eldritch.cheese@gmail.com>
2021-12-01 06:37:33 -0600

    Completed Day01

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -2,6 +2,8 @@
 use utils::Error;
 use utils::{Puzzle, PuzzleExtensions, PuzzleInput};
 
+use itertools::Itertools;
+
 pub struct Day01;
 
 impl Puzzle for Day01 {
@@ -13,14 +15,33 @@ impl Puzzle for Day01 {
     }
     fn part_1(&self) -> Result<Box<dyn std::fmt::Debug>, Error> {
         //let puzzle_input = self.puzzle_input(PuzzleInput::Example(0))?;
-        //let puzzle_input = self.puzzle_input(PuzzleInput::User)?;
-        let result = ();
+        let puzzle_input = self.puzzle_input(PuzzleInput::User)?;
+
+        let result = puzzle_input
+            .lines()
+            .map(|line| line.parse::<i32>().map_err(|err| err.into()))
+            .collect::<Result<Vec<_>, Error>>()?
+            .iter()
+            .tuple_windows()
+            .filter(|(a, b)| a < b)
+            .count();
         Ok(Box::new(result))
     }
     fn part_2(&self) -> Result<Box<dyn std::fmt::Debug>, Error> {
         //let puzzle_input = self.puzzle_input(PuzzleInput::Example(0))?;
-        //let puzzle_input = self.puzzle_input(PuzzleInput::User)?;
-        let result = ();
+        let puzzle_input = self.puzzle_input(PuzzleInput::User)?;
+
+        let result = puzzle_input
+            .lines()
+            .map(|line| line.parse::<i32>().map_err(|err| err.into()))
+            .collect::<Result<Vec<_>, Error>>()?
+            .iter()
+            .tuple_windows()
+            .map(|(a, b, c)| a + b + c)
+            .tuple_windows()
+            .filter(|(a, b)| a < b)
+            .count();
+
         Ok(Box::new(result))
     }
 }
