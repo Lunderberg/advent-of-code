9618071d3eadc38a3dbce639690919782eec6168
Eldritch Cheese <eldritch.cheese@gmail.com>
2021-12-14 08:50:42 -0600

    Completed day14.

--- src/solutions/day14.rs
+++ src/solutions/day14.rs
@@ -10,7 +10,9 @@ pub struct Day14;
 
 #[derive(Debug)]
 struct Polymer {
-    elements: String,
+    pair_counts: HashMap<(char, char), usize>,
+    first: char,
+    last: char,
 }
 
 #[derive(Debug)]
@@ -18,30 +20,57 @@ struct InsertionRules {
     rules: HashMap<(char, char), char>,
 }
 
+impl std::str::FromStr for Polymer {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Self, Error> {
+        let pair_counts = s.chars().tuple_windows::<(_, _)>().counts();
+        let first = s.chars().next().ok_or(Error::NoneError)?;
+        let last = s.chars().last().ok_or(Error::NoneError)?;
+        Ok(Self {
+            first,
+            last,
+            pair_counts,
+        })
+    }
+}
+
 impl Polymer {
     fn apply_rules(&self, rules: &InsertionRules) -> Self {
+        let pair_counts = self
+            .pair_counts
+            .iter()
+            .map(|(pair, counts)| (*pair, *counts))
+            .flat_map(|((a, b), counts)| {
+                rules
+                    .rules
+                    .get(&(a, b))
+                    .copied()
+                    .map(|insert| vec![(a, insert), (insert, b)].into_iter())
+                    .or_else(|| Some(vec![(a, b)].into_iter()))
+                    .unwrap()
+                    .map(move |pair| (pair, counts))
+            })
+            .into_group_map()
+            .into_iter()
+            .map(|(pair, count_vec)| (pair, count_vec.iter().sum::<usize>()))
+            .collect();
         Self {
-            elements: self
-                .elements
-                .chars()
-                .map(|element| Some(element))
-                .interleave(
-                    self.elements
-                        .chars()
-                        .tuple_windows()
-                        .map(|(a, b)| rules.rules.get(&(a, b)).copied()),
-                )
-                .flatten()
-                .collect(),
+            first: self.first,
+            last: self.last,
+            pair_counts,
         }
     }
 
     fn minmax_diff(&self) -> Result<usize, Error> {
-        self.elements
-            .chars()
-            .counts()
+        self.pair_counts
+            .iter()
+            .flat_map(|((a, b), counts)| {
+                vec![(*a, *counts), (*b, *counts)].into_iter()
+            })
+            .chain(vec![(self.first, 1), (self.last, 1)].into_iter())
+            .into_group_map()
             .into_iter()
-            .map(|(_k, v)| v)
+            .map(|(_element, counts)| counts.iter().sum::<usize>() / 2)
             .minmax()
             .into_option()
             .map(|(min, max)| max - min)
@@ -51,17 +80,15 @@ impl Polymer {
 
 impl Day14 {
     fn parse_inputs(&self) -> Result<(Polymer, InsertionRules), Error> {
-        let puzzle_input = self.puzzle_input(PuzzleInput::Example(0))?;
-        //let puzzle_input = self.puzzle_input(PuzzleInput::User)?;
+        //let puzzle_input = self.puzzle_input(PuzzleInput::Example(0))?;
+        let puzzle_input = self.puzzle_input(PuzzleInput::User)?;
 
         let mut line_iter = puzzle_input.lines();
-        let polymer = Polymer {
-            elements: line_iter
-                .by_ref()
-                .take_while(|line| line.len() > 0)
-                .map(|line| line.chars().collect())
-                .exactly_one()?,
-        };
+        let polymer = line_iter
+            .by_ref()
+            .take_while(|line| line.len() > 0)
+            .map(|line| line.parse::<Polymer>())
+            .exactly_one()??;
 
         let rules = InsertionRules {
             rules: line_iter
@@ -98,14 +125,17 @@ impl Puzzle for Day14 {
 
         let result = (0..10)
             .fold(polymer, |state, _| state.apply_rules(&rules))
-            .minmax_diff();
+            .minmax_diff()?;
 
         Ok(Box::new(result))
     }
     fn part_2(&self) -> Result<Box<dyn std::fmt::Debug>, Error> {
-        //let puzzle_input = self.puzzle_input(PuzzleInput::Example(0))?;
-        //let puzzle_input = self.puzzle_input(PuzzleInput::User)?;
-        let result = ();
+        let (polymer, rules) = self.parse_inputs()?;
+
+        let result = (0..40)
+            .fold(polymer, |state, _| state.apply_rules(&rules))
+            .minmax_diff()?;
+
         Ok(Box::new(result))
     }
 }
