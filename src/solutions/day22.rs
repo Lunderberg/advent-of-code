f6cd80a8f2071d5257f2a4e8f6d86977f13a9220
Eldritch Cheese <eldritch.cheese@gmail.com>
2021-12-22 21:09:21 -0600

    Day22, part 2 complete

--- src/solutions/day22.rs
+++ src/solutions/day22.rs
@@ -2,7 +2,9 @@
 use crate::utils::Error;
 use crate::utils::{Puzzle, PuzzleExtensions, PuzzleInput};
 
-use std::ops::RangeInclusive;
+use std::convert::TryInto;
+use std::marker::PhantomData;
+use std::ops::Range;
 use std::str::FromStr;
 
 use itertools::Itertools;
@@ -16,20 +18,150 @@ struct Command {
     region: Cuboid,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
+struct WorldState {
+    enabled_regions: Vec<Cuboid>,
+}
+
+#[derive(Debug, Clone)]
 struct Cuboid {
-    ranges: Vec<RangeInclusive<i64>>,
+    ranges: [Range<i64>; 3],
+}
+
+impl WorldState {
+    fn new() -> Self {
+        Self {
+            enabled_regions: Vec::new(),
+        }
+    }
+
+    fn after_command(&self, command: &Command) -> Self {
+        // Splitting out the untouched regions in necessary for
+        // performance reasons, because Cuboid.difference can split
+        // untouched regions.
+        let (untouched, touched): (Vec<_>, Vec<_>) = self
+            .enabled_regions
+            .iter()
+            .partition(|r| r.intersection(&command.region).is_empty());
+
+        let enabled_regions: Vec<Cuboid> = touched
+            .into_iter()
+            .flat_map(|r| r.difference(&command.region))
+            .chain(
+                std::iter::repeat_with(|| command.region.clone())
+                    .take(command.new_state as usize),
+            )
+            .chain(untouched.into_iter().cloned())
+            .collect();
+
+        Self { enabled_regions }
+    }
 }
 
 impl Cuboid {
-    fn contains(&self, loc: &Vec<i64>) -> bool {
-        loc.iter()
-            .zip(self.ranges.iter())
-            .all(|(coordinate, range)| range.contains(coordinate))
+    fn size(&self) -> u64 {
+        self.ranges
+            .iter()
+            .map(|r| {
+                if r.is_empty() {
+                    0
+                } else {
+                    (r.end - r.start) as u64
+                }
+            })
+            .product()
     }
 
-    fn locations(&self) -> impl Iterator<Item = Vec<i64>> + '_ {
-        self.ranges.iter().cloned().multi_cartesian_product()
+    // Return an iterator of cuboids whose union contains all points
+    // that are in self, but not in other.
+    fn difference<'a, 'b, 'c>(
+        &'a self,
+        other: &'b Self,
+    ) -> impl Iterator<Item = Self> + 'c
+    where
+        'a: 'c,
+        'b: 'c,
+    {
+        // I think this special case isn't needed, but let's see.
+        // // Special case, there's no overlap present, so just return
+        // // the original.
+        // let entirely_distinct =
+        //     self.ranges.iter().zip(other.ranges.iter()).any(
+        //         |(self_range, other_range)| {
+        //             (self_range.end < other_range.start)
+        //                 || (other_range.end < self_range.end)
+        //         },
+        //     );
+        // if entirely_distinct {
+        //     return vec![self.clone()].into_iter();
+        // }
+
+        // Determine the allowed region subsets.
+
+        self.ranges
+            .iter()
+            .zip(other.ranges.iter())
+            // Map to boolean of (inside_self, inside_other, region).
+            // Each list of ranges fills the region [a1,a2].
+            .map(|(self_range, other_range)| {
+                let a1 = self_range.start;
+                let a2 = self_range.end;
+                let b1 = other_range.start;
+                let b2 = other_range.end;
+
+                vec![a1, a2, b1, b2]
+                    .into_iter()
+                    .sorted()
+                    .tuple_windows()
+                    .filter(|(low, high)| low < high)
+                    .filter(move |(low, _high)| self_range.contains(&low))
+                    .map(move |(low, high)| {
+                        (other_range.contains(&low), low..high)
+                    })
+            })
+            .multi_cartesian_product()
+            // Filter out regions where all x/y/z coordinates overlap
+            // with the other region (inside_other is true for all
+            // dimensions).
+            .filter_map(|subregion| {
+                subregion
+                    .iter()
+                    .any(|(inside_other, _range)| !inside_other)
+                    .then(|| {
+                        let mut ranges = [0..0, 0..0, 0..0];
+
+                        subregion
+                            .into_iter()
+                            .map(|(_inside_other, range)| range)
+                            .zip_eq(ranges.iter_mut())
+                            .for_each(|(range, out)| {
+                                *out = range;
+                            });
+                        Cuboid { ranges }
+                    })
+            })
+    }
+
+    fn intersection(&self, other: &Self) -> Self {
+        let mut ranges = [0..0, 0..0, 0..0];
+
+        self.ranges
+            .iter()
+            .zip(other.ranges.iter())
+            .map(|(self_range, other_range)| {
+                let bottom = self_range.start.max(other_range.start);
+                let top = self_range.end.min(other_range.end);
+                bottom..top
+            })
+            .zip(ranges.iter_mut())
+            .for_each(|(val, out)| {
+                *out = val;
+            });
+        Self { ranges }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.ranges.iter().any(|r| r.is_empty())
     }
 }
 
@@ -62,12 +194,20 @@ impl FromStr for Command {
 
         let new_state = captures.name("state").unwrap().as_str() == "on";
 
-        let ranges = ["xmin", "xmax", "ymin", "ymax", "zmin", "zmax"]
+        let mut ranges = [0..0, 0..0, 0..0];
+        ["xmin", "xmax", "ymin", "ymax", "zmin", "zmax"]
             .iter()
             .map(|name| captures.name(name).unwrap().as_str().parse::<i64>())
             .tuples()
-            .map(|(a, b)| Ok((a?)..=(b?)))
-            .collect::<Result<_, Error>>()?;
+            .map(|(a, b)| -> Result<_, Error> { Ok((a?, b?)) })
+            // Problem gives inclusive range, but half-open ranges are
+            // easier to work with.
+            .map_ok(|(a, b)| a..(b + 1))
+            .zip(ranges.iter_mut())
+            .try_for_each(|(res, out)| -> Result<_, Error> {
+                *out = res?;
+                Ok(())
+            })?;
         let region = Cuboid { ranges };
 
         Ok(Self { new_state, region })
@@ -76,8 +216,9 @@ impl FromStr for Command {
 
 impl Day22 {
     fn parse_commands(&self) -> Result<Vec<Command>, Error> {
-        let puzzle_input = self.puzzle_input(PuzzleInput::Example(1))?;
-        //let puzzle_input = self.puzzle_input(PuzzleInput::User)?;
+        //let puzzle_input = self.puzzle_input(PuzzleInput::Example(1))?;
+        //let puzzle_input = self.puzzle_input(PuzzleInput::Example(2))?;
+        let puzzle_input = self.puzzle_input(PuzzleInput::User)?;
 
         let cuboids = puzzle_input
             .lines()
@@ -98,28 +239,37 @@ impl Puzzle for Day22 {
     fn part_1(&self) -> Result<Box<dyn std::fmt::Debug>, Error> {
         let commands = self.parse_commands()?;
 
-        let region = Cuboid {
-            ranges: vec![-50..=50, -50..=50, -50..=50],
+        let initialization_region = Cuboid {
+            ranges: [-50..51, -50..51, -50..51],
         };
 
-        let mut state: Vec<bool> = region.locations().map(|_| false).collect();
+        let final_state = commands
+            .into_iter()
+            .fold(WorldState::new(), |state, command| {
+                state.after_command(&command)
+            });
 
-        commands.iter().for_each(|command| {
-            region
-                .locations()
-                .map(|loc| command.region.contains(&loc))
-                .zip(state.iter_mut())
-                .filter_map(|(to_update, out)| to_update.then(|| out))
-                .for_each(|out| {
-                    *out = command.new_state;
-                });
-        });
-
-        let result = state.iter().filter(|b| **b).count();
+        let result = final_state
+            .enabled_regions
+            .iter()
+            .map(|region| region.intersection(&initialization_region).size())
+            .sum::<u64>();
         Ok(Box::new(result))
     }
     fn part_2(&self) -> Result<Box<dyn std::fmt::Debug>, Error> {
-        let result = ();
+        let commands = self.parse_commands()?;
+
+        let final_state = commands
+            .into_iter()
+            .fold(WorldState::new(), |state, command| {
+                state.after_command(&command)
+            });
+
+        let result = final_state
+            .enabled_regions
+            .iter()
+            .map(|region| region.size())
+            .sum::<u64>();
         Ok(Box::new(result))
     }
 }
