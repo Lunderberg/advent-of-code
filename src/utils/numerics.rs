e9bb31b9c61773e1161985b2bbe2f7ed5cb9d8f1
Eldritch Cheese <eldritch.cheese@gmail.com>
2022-01-03 19:44:54 -0600

    Added least-common multiple.

--- src/utils/numerics.rs
+++ src/utils/numerics.rs
@@ -2,6 +2,31 @@ pub fn gcd(a: i32, b: i32) -> i32 {
     if b == 0 {
         a
     } else {
-        gcd(b, a % b)
+        gcd(b, a.rem_euclid(b))
+    }
+}
+
+pub fn lcm(a: i32, b: i32) -> i32 {
+    a * b / gcd(a, b)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_gcd() {
+        assert_eq!(gcd(10, 8), 2);
+        assert_eq!(gcd(13, 17), 1);
+        assert_eq!(gcd(13, -35), 1);
+        assert_eq!(gcd(-4000, 35), 5);
+    }
+
+    #[test]
+    fn test_lcm() {
+        assert_eq!(lcm(10, 8), 40);
+        assert_eq!(lcm(13, 17), 221);
+        assert_eq!(lcm(13, -35), -455);
+        assert_eq!(lcm(-4000, 35), -28000);
     }
 }
