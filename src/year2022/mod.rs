b7b57261f251db07947f5b8f8da0a3a19bade80e
Eldritch Cheese <eldritch.cheese@gmail.com>
2022-12-01 05:57:00 -0600

    Start of a build.rs to make it easier to add new solution files

--- src/year2022/mod.rs
+++ src/year2022/mod.rs
@@ -1,7 +1,8 @@
-mod day01;
+include!(concat!(env!("OUT_DIR"), "/generated_year2022.rs"));
+// mod day01;
 
-use crate::framework::{PuzzleRunner, PuzzleRunnerImpl};
+// use crate::framework::{PuzzleRunner, PuzzleRunnerImpl};
 
-pub fn solutions() -> impl Iterator<Item = Box<dyn PuzzleRunner>> {
-    vec![PuzzleRunnerImpl::<day01::ThisPuzzle>::new_box()].into_iter()
-}
+// pub fn solutions() -> impl Iterator<Item = Box<dyn PuzzleRunner>> {
+//     vec![PuzzleRunnerImpl::<day01::ThisPuzzle>::new_box()].into_iter()
+// }
