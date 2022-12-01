4b23771a8ba01388571042c725c5f025e895911c
Eldritch Cheese <eldritch.cheese@gmail.com>
2022-12-01 06:23:24 -0600

    Solved 2022, day 1

--- src/year2022/day01.rs
+++ src/year2022/day01.rs
@@ -8,26 +8,44 @@ pub struct ThisPuzzle;
 impl Puzzle for ThisPuzzle {
     const YEAR: u32 = 2022;
     const DAY: u8 = 1;
-    const IMPLEMENTED: bool = false;
+    const IMPLEMENTED: bool = true;
     const EXAMPLE_NUM: u8 = 0;
 
-    type ParsedInput = Vec<i32>;
+    type ParsedInput = Vec<Vec<i32>>;
     fn parse_input<'a>(
-        lines: impl Iterator<Item = &'a str>,
+        mut lines: impl Iterator<Item = &'a str>,
     ) -> Result<Self::ParsedInput, Error> {
-        // lines
-        //     .map(|line| line.parse::<i32>().map_err(|err| err.into()))
-        //     .collect()
-        todo!()
+        std::iter::from_fn(move || -> Option<Result<Vec<i32>, Error>> {
+            let group: Result<Vec<_>, _> = lines
+                .by_ref()
+                .take_while(|line| !line.is_empty())
+                .map(|line| line.parse::<i32>())
+                .collect();
+            match group {
+                Ok(v) if v.is_empty() => None,
+                Ok(v) => Some(Ok(v)),
+                Err(err) => Some(Err(err.into())),
+            }
+        })
+        .collect()
     }
 
-    type Part1Result = usize;
+    type Part1Result = i32;
     fn part_1(values: &Self::ParsedInput) -> Result<Self::Part1Result, Error> {
-        todo!()
+        Ok(values
+            .iter()
+            .map(|elf| elf.iter().sum::<i32>())
+            .max()
+            .unwrap())
     }
 
-    type Part2Result = usize;
+    type Part2Result = i32;
     fn part_2(values: &Self::ParsedInput) -> Result<Self::Part2Result, Error> {
-        todo!()
+        Ok(values
+            .iter()
+            .map(|elf| -elf.iter().sum::<i32>())
+            .k_smallest(3)
+            .map(|elf| -elf)
+            .sum::<i32>())
     }
 }
