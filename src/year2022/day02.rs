413ec5d8da0742784fd15e97424ebd52414bfdeb
Eldritch Cheese <eldritch.cheese@gmail.com>
2022-12-02 08:07:11 -0600

    Cleanup of 2022-12-02
    No need for the class structure when it could be done with modular
    arithmetic.

--- src/year2022/day02.rs
+++ src/year2022/day02.rs
@@ -5,114 +5,22 @@ use itertools::Itertools;
 
 pub struct ThisDay;
 
-#[derive(PartialEq, Clone, Copy)]
-enum Shape {
-    Rock,
-    Paper,
-    Scissors,
-}
-
-enum Col2 {
-    X,
-    Y,
-    Z,
-}
-
-impl Shape {
-    fn score(&self) -> i32 {
-        match self {
-            Shape::Rock => 1,
-            Shape::Paper => 2,
-            Shape::Scissors => 3,
-        }
-    }
-
-    fn to_cause(&self, outcome: RoundResult) -> Shape {
-        use RoundResult::*;
-        use Shape::*;
-        match (outcome, self) {
-            (Draw, _) => *self,
-            (Win, Rock) => Paper,
-            (Win, Paper) => Scissors,
-            (Win, Scissors) => Rock,
-            (Loss, Rock) => Scissors,
-            (Loss, Paper) => Rock,
-            (Loss, Scissors) => Paper,
-        }
-    }
-}
-
-#[derive(Copy, Clone)]
-enum RoundResult {
-    Win,
-    Loss,
-    Draw,
-}
-
-impl RoundResult {
-    fn score(&self) -> i32 {
-        match self {
-            RoundResult::Win => 6,
-            RoundResult::Draw => 3,
-            RoundResult::Loss => 0,
-        }
-    }
-}
-
-struct Round {
-    player: Shape,
-    opponent: Shape,
-}
-
-impl Round {
-    fn result(&self) -> RoundResult {
-        use RoundResult::*;
-        use Shape::*;
-        match (&self.player, &self.opponent) {
-            (a, b) if a == b => Draw,
-            (Rock, Scissors) => Win,
-            (Scissors, Paper) => Win,
-            (Paper, Rock) => Win,
-            _ => Loss,
-        }
-    }
-
-    fn score(&self) -> i32 {
-        self.player.score() + self.result().score()
-    }
-}
-
 pub struct StrategyGuide {
-    opponent: Shape,
-    col2: Col2,
+    opponent: i32,
+    col2: i32,
 }
 
 impl StrategyGuide {
-    fn col2_as_rock_paper_scissors(&self) -> Round {
-        let player = match self.col2 {
-            Col2::X => Shape::Rock,
-            Col2::Y => Shape::Paper,
-            Col2::Z => Shape::Scissors,
-        };
-        Round {
-            player,
-            opponent: self.opponent,
-        }
+    fn part1_score(&self) -> i32 {
+        let player = self.col2;
+        let outcome = (player - self.opponent + 1).wrapping_rem_euclid(3);
+        (player + 1) + 3 * outcome
     }
 
-    fn col2_as_win_loss_draw(&self) -> Round {
-        let outcome = match self.col2 {
-            Col2::X => RoundResult::Loss,
-            Col2::Y => RoundResult::Draw,
-            Col2::Z => RoundResult::Win,
-        };
-
-        let player = self.opponent.to_cause(outcome);
-
-        Round {
-            player,
-            opponent: self.opponent,
-        }
+    fn part2_score(&self) -> i32 {
+        let outcome = self.col2;
+        let player = (outcome + self.opponent - 1).wrapping_rem_euclid(3);
+        (player + 1) + 3 * outcome
     }
 }
 
@@ -120,23 +28,21 @@ impl std::str::FromStr for StrategyGuide {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        use Shape::*;
-
         let mut chars = s.chars();
 
         let opponent = match chars.next().ok_or(Error::NoneError)? {
-            'A' => Ok(Rock),
-            'B' => Ok(Paper),
-            'C' => Ok(Scissors),
+            'A' => Ok(0),
+            'B' => Ok(1),
+            'C' => Ok(2),
             c => Err(Error::UnknownChar(c)),
         }?;
 
         chars.next();
 
         let col2 = match chars.next().ok_or(Error::NoneError)? {
-            'X' => Ok(Col2::X),
-            'Y' => Ok(Col2::Y),
-            'Z' => Ok(Col2::Z),
+            'X' => Ok(0),
+            'Y' => Ok(1),
+            'Z' => Ok(2),
             c => Err(Error::UnknownChar(c)),
         }?;
 
@@ -158,19 +64,11 @@ impl Puzzle for ThisDay {
 
     type Part1Result = i32;
     fn part_1(values: &Self::ParsedInput) -> Result<Self::Part1Result, Error> {
-        Ok(values
-            .iter()
-            .map(|guide| guide.col2_as_rock_paper_scissors())
-            .map(|round| round.score())
-            .sum())
+        Ok(values.iter().map(|guide| guide.part1_score()).sum())
     }
 
     type Part2Result = i32;
     fn part_2(values: &Self::ParsedInput) -> Result<Self::Part2Result, Error> {
-        Ok(values
-            .iter()
-            .map(|guide| guide.col2_as_win_loss_draw())
-            .map(|round| round.score())
-            .sum())
+        Ok(values.iter().map(|guide| guide.part2_score()).sum())
     }
 }
