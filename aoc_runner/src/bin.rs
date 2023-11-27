a646472b2ce52e3ee33e0e33bb872aab89a23fbe
Eldritch Cheese <eldritch.cheese@gmail.com>
2023-11-26 20:29:09 -0600

    Broke out 2022 into an independent crate

--- aoc_runner/src/bin.rs
+++ aoc_runner/src/bin.rs
@@ -23,6 +23,7 @@ struct Options {
 fn main() -> Result<(), Error> {
     let runners: Vec<Box<dyn PuzzleRunner>> = std::iter::empty()
         .chain(aoc_year2021::solutions())
+        .chain(aoc_year2022::solutions())
         .chain(aoc_framework::solutions())
         .collect();
 
