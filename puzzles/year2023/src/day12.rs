451a1f30482eacce05e0f3196e9503674c64bbcf
Eldritch Cheese <eldritch.cheese@gmail.com>
2023-12-14 21:31:59 -0600

    Day12, now completes faster, but with a wrong answer. Improvement?

--- puzzles/year2023/src/day12.rs
+++ puzzles/year2023/src/day12.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, str::FromStr};
+use std::{collections::HashMap, fmt::Display, str::FromStr};
 
 use aoc_utils::prelude::*;
 
@@ -56,340 +56,117 @@ impl Display for Record {
     }
 }
 
-impl DirectedGraph<(Vec<bool>, u8, u8)> for Record {
-    fn connections_from<'a>(
-        &'a self,
-        (node, num_true, num_false): &'a (Vec<bool>, u8, u8),
-    ) -> impl Iterator<Item = (Vec<bool>, u8, u8)> + '_ {
-        [true, false]
-            .into_iter()
-            .filter(|_| node.len() < self.springs.len())
-            .filter(|&b| {
-                // self.springs[node.len()]
-                //     .map(|known| known == *b)
-                //     .unwrap_or(true)
-                match self.springs[node.len()] {
-                    Some(true) => b,
-                    Some(false) => !b,
-                    None => (b && *num_true > 0) || (!b && *num_false > 0),
-                    // None => true,
-                    // None => (!b) || (b && *num_true > 0),
-                    // None => (b) || ((!b) && *num_false > 0),
-                }
-            })
-            .filter(|b| {
-                self.is_valid_start(
-                    node.iter().cloned().chain(std::iter::once(*b)),
-                )
-            })
-            .map(move |b| -> (Vec<bool>, u8, u8) {
-                let is_new = self.springs[node.len()].is_none();
-                let num_true = num_true.saturating_sub((b && is_new) as u8);
-                let num_false =
-                    num_false.saturating_sub(((!b) && is_new) as u8);
-
-                (
-                    node.iter().cloned().chain(std::iter::once(b)).collect(),
-                    num_true,
-                    num_false,
-                )
-            })
-    }
-}
-
 impl Record {
-    fn iter_naive(
-        &self,
-    ) -> impl Iterator<Item = impl Iterator<Item = bool> + '_> {
-        let num_unknown = self
-            .springs
-            .iter()
-            .filter(|spring| spring.is_none())
-            .count();
-
-        (0..u64::pow(2, num_unknown as u32)).map(|i| self.iter_spring_guess(i))
-    }
-
-    fn iter_spring_guess(&self, mut i: u64) -> impl Iterator<Item = bool> + '_ {
-        self.springs.iter().map(move |spring| {
-            spring.unwrap_or_else(|| {
-                let val = i % 2 == 0;
-                i = i >> 1;
-                val
-            })
-        })
-    }
-
     fn count_possible(&self) -> usize {
-        let n = self.springs.len();
-        let mut count = 0;
-        let mut state = vec![false; n];
-
-        // let pbar = indicatif::ProgressBar::new(u32::MAX as u64);
-
-        // let mut value = 0;
-        // let mut old_state = state.clone();
-        loop {
-            // value = {
-            //     let new_value = state
-            //         .iter()
-            //         .rev()
-            //         .skip(n.saturating_sub(32))
-            //         .rev()
-            //         .fold(0, |a, b| 2 * a + (*b as u64));
-            //     // if new_value < value {
-            //     //     println!(
-            //     //         "Old state: {}",
-            //     //         old_state
-            //     //             .iter()
-            //     //             .map(|b| if *b { '#' } else { '.' })
-            //     //             .join("")
-            //     //     );
-            //     //     println!(
-            //     //         "New state: {}",
-            //     //         state
-            //     //             .iter()
-            //     //             .map(|b| if *b { '#' } else { '.' })
-            //     //             .join("")
-            //     //     );
-            //     // }
-            //     // assert!(
-            //     //     new_value >= value,
-            //     //     "Value changed from {value} to {new_value}"
-            //     // );
-            //     // old_state = state.clone();
-            //     new_value
-            // };
-            // pbar.set_position(value);
-            // println!(
-            //     "\tChecking {}",
-            //     state.iter().map(|b| if *b { '#' } else { '.' }).join("")
-            // );
-            if let Some(i_bit) = self.first_impossible_bit(&state) {
-                if state[i_bit] {
-                    let mut found_false_bit = false;
-                    for i in (0..i_bit).rev() {
-                        if let Some(known_bit) = self.springs[i] {
-                            state[i] = known_bit;
-                        } else if state[i] {
-                            state[i] = false;
-                        } else {
-                            state[i] = true;
-                            found_false_bit = true;
-                            break;
-                        }
-                    }
-                    if !found_false_bit {
-                        break;
-                    }
-                }
-
-                state[i_bit] = !state[i_bit];
-
-                for i in (i_bit + 1)..n {
-                    state[i] = self.springs[i].unwrap_or(false);
-                }
-            } else {
-                // println!("\tThis state is legal");
-                count += 1;
-
-                let mut found_false_bit = false;
-                for i in (0..n).rev() {
-                    if let Some(known_bit) = self.springs[i] {
-                        state[i] = known_bit;
-                    } else if state[i] {
-                        state[i] = false;
-                    } else {
-                        state[i] = true;
-                        found_false_bit = true;
-                        break;
-                    }
-                }
-                if !found_false_bit {
-                    break;
-                }
-            }
-        }
-
-        // pbar.finish();
-
-        count
+        let mut cache = HashMap::new();
+        Self::count_possible_impl(&self.springs, &self.groups, &mut cache)
     }
 
-    fn first_impossible_bit(&self, bits: &[bool]) -> Option<usize> {
-        assert_eq!(bits.len(), self.springs.len());
+    fn count_possible_impl(
+        springs: &[Option<bool>],
+        groups: &[u8],
+        cache: &mut HashMap<(Vec<Option<bool>>, Vec<u8>), usize>,
+    ) -> usize {
+        let key = (
+            springs.iter().cloned().collect(),
+            groups.iter().cloned().collect(),
+        );
 
-        let mut i_current_group = 0;
-        let mut current_streak: Option<u8> = None;
-
-        let total_num_true = self.groups.iter().sum::<u8>();
-        let total_num_false = (self.springs.len() as u8) - total_num_true;
-
-        let mut num_true = 0;
-        let mut num_false = 0;
-
-        for (i, (bit, spring)) in
-            bits.iter().zip(self.springs.iter()).enumerate()
-        {
-            // If there is a mismatch with a known bit
-            if let Some(known) = spring {
-                if bit != known {
-                    // println!(
-                    //     "\t\tFirst impossible bit is {i}, \
-                    //      due to mismatch with known"
-                    // );
-                    return Some(i);
-                }
-            }
-
-            if *bit {
-                num_true += 1;
-                if num_true > total_num_true {
-                    // println!(
-                    //     "\t\tFirst impossible bit is {i}, \
-                    //      due to too many true bits."
-                    // );
-                    return Some(i);
-                }
-            } else {
-                num_false += 1;
-                if num_false > total_num_false {
-                    // println!(
-                    //     "\t\tFirst impossible bit is {i}, \
-                    //      due to too many false bits."
-                    // );
-                    return Some(i);
-                }
-            }
-
-            if *bit {
-                if let Some(current_streak) = &mut current_streak {
-                    *current_streak += 1;
-                } else {
-                    current_streak = Some(1);
-                    if i_current_group >= self.groups.len() {
-                        // println!(
-                        //     "\t\tFirst impossible bit is {i}, \
-                        //      due to exceeding number of groups {}",
-                        //     self.groups.len()
-                        // );
-                        return Some(i);
-                    }
-                }
-                if current_streak.unwrap() > self.groups[i_current_group] {
-                    // println!(
-                    //     "\t\tFirst impossible bit is {i}, \
-                    //      due to streak {} \
-                    //      exceeding length of group {i_current_group}",
-                    //     current_streak.unwrap()
-                    // );
-                    return Some(i);
-                }
-            } else {
-                if let Some(current_streak) = &current_streak {
-                    if current_streak != &self.groups[i_current_group] {
-                        // println!(
-                        //     "\t\tFirst impossible bit is {i}, \
-                        //      due to streak {} ending \
-                        //      without correct group length {}",
-                        //     current_streak, self.groups[i_current_group]
-                        // );
-                        return Some(i);
-                    }
-                    i_current_group += 1;
-                }
-                current_streak = None;
-            }
+        if let Some(&cached) = cache.get(&key) {
+            return cached;
         }
-        None
-    }
 
-    fn count_with_early_backtrack(&self) -> (usize, usize) {
-        let group_sum = self.groups.iter().sum::<u8>();
-        let (known_true, known_false) =
-            self.springs
-                .iter()
-                .fold((0, 0), |(count_t, count_f), spring| match spring {
-                    Some(true) => (count_t + 1, count_f),
-                    Some(false) => (count_t, count_f + 1),
-                    None => (count_t, count_f),
-                });
-
-        let mut total_scanned = 0;
-        let result = self
-            .iter_depth_first([(
-                Vec::new(),
-                group_sum - known_true,
-                (self.springs.len() as u8) - group_sum - known_false,
-            )])
-            .map(|(seq, _, _)| seq)
-            .inspect(|_| {
-                total_scanned += 1;
-            })
-            .filter(|seq| seq.len() == self.springs.len())
-            .filter(|seq| self.is_valid_seq(seq.clone()))
-            // .inspect(|seq| {
-            //     println!(
-            //         "\t{}",
-            //         seq.iter()
-            //             .map(|b| match b {
-            //                 true => '.',
-            //                 false => '#',
-            //             })
-            //             .join("")
-            //     )
-            // })
+        let remaining_group_sum =
+            groups.iter().map(|g| *g as usize).sum::<usize>();
+        let potential_true_values = springs
+            .iter()
+            .filter(|spring| spring.unwrap_or(true))
             .count();
-        // println!("\tScanned {total_scanned} states");
-        (result, total_scanned)
-    }
 
-    fn is_valid_start(
-        &self,
-        spring_listing: impl IntoIterator<Item = bool>,
-    ) -> bool {
-        let mut iter = spring_listing.into_iter();
+        // let spring_str = springs
+        //     .iter()
+        //     .map(|spring| match spring {
+        //         None => '?',
+        //         Some(true) => '#',
+        //         Some(false) => '.',
+        //     })
+        //     .join("");
+        // let group_str = groups.iter().join(", ");
 
-        self.groups.iter().all(|group_size| {
-            let first = iter.next();
-            if first.is_none() {
-                return true;
-            }
+        let result = if potential_true_values < remaining_group_sum {
+            // println!(
+            //     "Sequence {spring_str} doesn't have enough true values \
+            //      for {group_str}"
+            // );
+            0
+        } else if let Some(&group) = groups.get(0) {
+            let group = group as usize;
 
-            let found_group = std::iter::once(first.unwrap())
-                .chain(iter.by_ref())
-                .skip_while(|b| !b)
-                .take_while(|b| *b)
-                .count() as u8;
-            found_group == *group_size
-                || (found_group < *group_size && iter.next().is_none())
-        })
-    }
+            // For all regions that could fit a group of
+            (0..=springs.len() - group)
+                // So long as we don't skip past a known true value,
+                .take_while_inclusive(|&i| match springs[i] {
+                    None => true,
+                    Some(false) => true,
+                    Some(true) => false,
+                })
+                // all items in the group may be true,
+                .filter(|i| {
+                    (0..group).all(|offset| match springs[i + offset] {
+                        None => true,
+                        Some(true) => true,
+                        Some(false) => {
+                            // println!(
+                            //     "Placing group of size {group} \
+                            //      at location {i} \
+                            //      in sequences {spring_str} \
+                            //      would hit false value after {offset}"
+                            // );
+                            false
+                        }
+                    })
+                })
+                // and the item after the group is either the end or may
+                // contain a false value,
+                .filter(|i| match springs.get(i + group) {
+                    None => true,
+                    Some(None) => true,
+                    Some(Some(true)) => {
+                        // println!(
+                        //     "Placing group of size {group} \
+                        //      at location {i} \
+                        //      in sequences {spring_str} \
+                        //      would be immediately followed by true"
+                        // );
+                        false
+                    }
+                    Some(Some(false)) => true,
+                })
+                // then recursively count the number of possible arrangements
+                .map(|i| {
+                    let earliest_next_group =
+                        usize::min(i + group + 1, springs.len());
+                    // println!(
+                    //     "Sequence {spring_str} \
+                    //      could have first group of size {group} \
+                    //      starting at index {i}."
+                    // );
+                    Self::count_possible_impl(
+                        &springs[earliest_next_group..],
+                        &groups[1..],
+                        cache,
+                    )
+                })
+                // and sum over all possible locations
+                .sum()
+        } else {
+            // println!("Empty sequence {spring_str} with no groups {group_str}");
+            1
+        };
 
-    fn is_valid_seq(
-        &self,
-        spring_listing: impl IntoIterator<Item = bool>,
-    ) -> bool {
-        spring_listing
-            .into_iter()
-            .group_sizes()
-            .zip_longest(self.groups.iter().cloned())
-            .all(|pair| match pair {
-                itertools::EitherOrBoth::Both(a, b) => a == b,
-                _ => false,
-            })
-    }
+        cache.insert(key, result);
 
-    fn num_arrangements(&self) -> usize {
-        self.iter_naive()
-            .filter_map(|seq| {
-                if self.is_valid_seq(seq) {
-                    Some(())
-                } else {
-                    None
-                }
-            })
-            .count()
+        result
     }
 
     fn unfold(&self) -> Self {
@@ -406,36 +183,6 @@ impl Record {
             .collect();
         let groups = [&self.groups; 5].into_iter().flatten().cloned().collect();
         Self { springs, groups }
-    }
-}
-
-trait IterBoolExt {
-    fn group_sizes(self) -> impl Iterator<Item = u8>;
-}
-
-impl<Iter> IterBoolExt for Iter
-where
-    Iter: IntoIterator<Item = bool>,
-{
-    fn group_sizes(self) -> impl Iterator<Item = u8> {
-        let mut iter = self.into_iter();
-        std::iter::from_fn(move || {
-            let first = iter.next()?;
-
-            let group_size = std::iter::once(first)
-                .chain(iter.by_ref())
-                .skip_while(|b| !b)
-                .take_while(|b| *b)
-                .count() as u8;
-
-            if group_size == 0 {
-                None
-            } else {
-                Some(group_size)
-            }
-
-            // Some(group_size)
-        })
     }
 }
 
